3350d66d7ebe83f8aa2f106af5a10db9b952fbc8
Mitchell Ryrie <liuxiaowen66621@gmail.com>
2024-08-31 23:40:42 +0800

    feat: 增加新组件
    Signed-off-by: Mitchell Ryrie <liuxiaowen66621@gmail.com>

---
+++ ViewTransitionWrapper/ViewTransitionWrapper/ViewTransitionWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTransitionWrapperApp.swift
+//  ViewTransitionWrapper
+//
+//  Created by L Af on 2024/8/31.
+//
+
+import SwiftUI
+
+@main
+struct ViewTransitionWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
